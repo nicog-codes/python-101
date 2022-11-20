91834f1f18d58f83e2e3396fd1ffe7bd10166794
nicog-codes <118675392+nicog-codes@users.noreply.github.com>
2022-11-20 22:43:17 +0100

    Update grades.py

--- 3-control-flow/grades.py
+++ 3-control-flow/grades.py
@@ -1,9 +1,11 @@
 # Grades 💯
 # Codédex
 
-grade = 58
+import random
 
-if grade >= 55:
-  print('You passed.')
+grade = random.randrange (0 , 255)   #  will give us a random number that is between 0 and 255
+print(grade) #optional
+if grade > 55:
+  print("You passed.")
 else:
-  print('You failed.')
+  print("You failed.")
